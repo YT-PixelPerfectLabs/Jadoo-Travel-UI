4063d6f3efbbcaf8016d56a83d9d4631c1147cec
ThePixelPerfect <fedevsandeep@gmail.com>
2024-11-22 23:29:45 +0530

    Added header navigation with responsive styling

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const headerMenu = document.querySelector("#header");
+const burgerMenu = headerMenu.querySelector(".burger");
+const closeMenu = headerMenu.querySelector(".close-menu");
+const headerBackdrop = headerMenu.querySelector(".header-backdrop");
+
+// Open Close Navbar Menu on Click Burger
+if (burgerMenu && headerMenu) {
+  burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("is-active");
+    headerMenu.classList.toggle("menu-is-active");
+    document.body.classList.toggle("overflow-hidden");
+  });
+
+  closeMenu.addEventListener("click", () => {
+    burgerMenu.classList.remove("is-active");
+    headerMenu.classList.remove("menu-is-active");
+    document.body.classList.remove("overflow-hidden");
+  });
+
+  headerBackdrop.addEventListener("click", () => {
+    burgerMenu.classList.remove("is-active");
+    headerMenu.classList.remove("menu-is-active");
+    document.body.classList.remove("overflow-hidden");
+  });
+}
+
+// Change Header Background on Scrolling
+window.addEventListener("scroll", () => {
+  if (this.scrollY >= 85) {
+    headerMenu.classList.add("on-scroll");
+  } else {
+    headerMenu.classList.remove("on-scroll");
+  }
+});
