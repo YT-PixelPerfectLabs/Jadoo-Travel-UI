40a2b3b67cc8a11880de06297c2ec72d5483458b
ThePixelPerfect <fedevsandeep@gmail.com>
2024-11-24 00:09:38 +0530

    Added testimonials section with responsive styling

--- js/script.js
+++ js/script.js
@@ -32,3 +32,40 @@ window.addEventListener("scroll", () => {
     headerMenu.classList.remove("on-scroll");
   }
 });
+
+new Swiper(".testimonials-swiper", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  loop: true,
+  clickable: true,
+  allowTouchMove: true,
+  spaceBetween: 24,
+  initialSlide: 0,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      direction: "horizontal",
+      slidesPerView: 1.1,
+      spaceBetween: 24,
+      allowTouchMove: true,
+      initialSlide: 0,
+    },
+    992: {
+      direction: "vertical",
+      slidesPerView: 1.5,
+      spaceBetween: 0,
+      allowTouchMove: false,
+      initialSlide: 2,
+    },
+  },
+});
